0e52c8806b14af762dd881365c7783c10aac5b14
Hannah <hkriggs128@gmail.com>
2015-03-02 11:37:29 -0500

    location

---
+++ src/Venue.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.awt.*;
+/**
+ * Write a description of class Venue here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Venue
+{
+    private ArrayList<Location> locations; 
+    
+    /**
+     * Constructor for objects of class Venue
+     */
+    public Venue() {
+        locations = new ArrayList<Location>(); 
+    }
+    
+    /**
+     * Add a location to the venue.
+     * 
+     * @param  name    the name of the location
+     * @param  x       the x-coordinate of the location
+     * @param  y       the y-coordinate of the location
+     */
+    public void addLocation(String name, int x, int y) {
+        locations.add(new Location(name, x, y));
+    }
+    
+     /**
+     * Draws the Venue object.
+     * 
+     * @param  g    the Graphics2D object
+     */
+    public void draw(Graphics2D g) {
+        for (Location location : locations) {
+            location.draw(g); 
+        }
+    }
+}
